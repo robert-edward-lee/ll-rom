67c49fb834f526ec4c6d481d8d77937593bb8087
s38449 <r.bolgov@kmz.su>
2022-07-01 11:32:51 +0300

    Добавлено автовыравнивание печати и рефакторинг

--- src/ll.c
+++ src/ll.c
@@ -16,6 +16,7 @@
 #include "service_printing.h"
 #include "version.h"
 
+#define MAX_REC_COUNT 40
 
 typedef enum
 {
@@ -42,30 +43,30 @@ char DirPath[PATH_MAX] = ".";
 
 const char ABOUT[] = "ll v." VERSION "\n";
 
-opts_et getOpts(int argc, const char* argv[], opts_et* opts);
+int linkAlign = 0;
+int userAlign = 0;
+int groupAlign = 0;
+int sizeAlign = 0;
 
-void tolowerWord(char* word);
 
+opts_et getOpts(int argc, const char *argv[], opts_et *opts);
+void tolowerWord(char *word);
 int sortDirent(const struct dirent** dirent1, const struct dirent** dirent2);
-
-struct stat handleLink(char* path);
-
-void printStat(const file_info_st* file_info);
-
-int putGayText(char* out_text, const char* in_text, char file_type, int exec_type);
-
-char getFileType(const char* path);
-
-void signalHandler(int signal, siginfo_t* signalInfo, void* userContext);
-
+struct stat handleLink(char *path);
+void printStat(const file_info_st *file_info);
+int putGayText(char *out_text, const char *in_text, char file_type, int exec_type);
+char getFileType(const char *path);
+void signalHandler(int signal, siginfo_t *signalInfo, void *userContext);
 void signalCatcher(void);
+void setAlignment(const file_info_st *file_info);
 
-int main(int argc, const char* argv[])
+
+int main(int argc, const char *argv[])
 {
     opts_et opts;
     struct dirent** name_list;
     int names, i;
-    file_info_st* data_files;
+    file_info_st *data_files;
 
     signalCatcher();
 
@@ -111,6 +112,11 @@ int main(int argc, const char* argv[])
 
     for (i = 0; i < names; i++)
     {
+        setAlignment(&data_files[i]);
+    }
+
+    for (i = 0; i < names; i++)
+    {
         printStat(&data_files[i]);
     }
 
@@ -120,7 +126,7 @@ ret_err:
     return -1;
 }
 
-opts_et getOpts(int argc, const char* argv[], opts_et* opts)
+opts_et getOpts(int argc, const char *argv[], opts_et *opts)
 {
     int ret = 0;
     struct stat dir_stat;
@@ -149,36 +155,67 @@ int sortDirent(const struct dirent** dirent1, const struct dirent** dirent2)
 {
     char file_one[NAME_MAX];
     char file_two[NAME_MAX];
+    char *file_one_p = file_one;
+    char *file_two_p = file_two;
+    size_t i;
 
     memcpy(file_one, (**dirent1).d_name, NAME_MAX);
     memcpy(file_two, (**dirent2).d_name, NAME_MAX);
 
     tolowerWord(file_one);
     tolowerWord(file_two);
-    /* костыль: переписать нормальное сравнение выкинув из сравнения все не буквы */
-    return strcmp(isalpha(file_one[0]) ? file_one : (file_one + 1), isalpha(file_two[0]) ? file_two : (file_two + 1));
+
+    for(i = 0; i != NAME_MAX; i++)
+    {
+        if(isalpha(file_one[i]))
+        {
+            file_one_p = &file_one[i];
+            break;
+        }
+    }
+
+    for(i = 0; i != NAME_MAX; i++)
+    {
+        if(isalpha(file_two[i]))
+        {
+            file_two_p = &file_two[i];
+            break;
+        }
+    }
+
+    return strncmp(file_one_p, file_two_p, NAME_MAX);
 }
 
-struct stat handleLink(char* path)
+struct stat handleLink(char *path)
 {
+    static int rec_count = 0;
     struct stat ret_stat;
-    char link[PATH_MAX];
+    char link[PATH_MAX] = {0};
+
+    DbgPrint("%s -> ", path);
+    if (rec_count == MAX_REC_COUNT)
+    {
+        PrintWarn("Too many nested symbolic link: %s", path);
+        exit(EXIT_FAILURE);
+    }
 
     if (getFileType(path) == 'l')
     {
         readlink(path, link, PATH_MAX);
-        sprintf(path, "%s", link);
+        sprintf(path, link);
         memset(link, 0, PATH_MAX);
+        rec_count++;
         handleLink(path);
     }
     else
     {
         lstat(path, &ret_stat);
+        rec_count = 0;
     }
     return ret_stat;
 }
 
-void tolowerWord(char* word)
+void tolowerWord(char *word)
 {
     int i = 0;
 
@@ -189,12 +226,15 @@ void tolowerWord(char* word)
     }
 }
 
-void printStat(const file_info_st* file_info)
+void printStat(const file_info_st *file_info)
 {
     char file_type;
     char text[PATH_MAX];
-    char* text_offset = text;
-    struct tm* local_time;
+    char *text_offset = text;
+    struct tm local_time, global_time;
+
+    time_t tmp_t = time(NULL);
+    global_time = *localtime(&tmp_t);
 
     /* печать типа файла */
     file_type = getFileType(file_info->name);
@@ -208,17 +248,26 @@ void printStat(const file_info_st* file_info)
     text_offset += sprintf(text_offset, "%s", (file_info->f_stat.st_mode & S_IXGRP) ? "x" : "-");
     text_offset += sprintf(text_offset, "%s", (file_info->f_stat.st_mode & S_IROTH) ? "r" : "-");
     text_offset += sprintf(text_offset, "%s", (file_info->f_stat.st_mode & S_IWOTH) ? "w" : "-");
-    text_offset += sprintf(text_offset, "%s ", (file_info->f_stat.st_mode & S_IXOTH) ? "x" : "-");
+    text_offset += sprintf(text_offset, "%s", (file_info->f_stat.st_mode & S_IXOTH) ? "x" : "-");
+    text_offset += sprintf(text_offset, " ");
     /* печать числа символьных ссылок */
-    text_offset += sprintf(text_offset, "%2ld ", file_info->f_stat.st_nlink);
+    text_offset += sprintf(text_offset,
+#ifdef ARM
+        "%*d ",
+#else
+        "%*ld ",
+#endif
+        linkAlign, file_info->f_stat.st_nlink);
     /* печать пользователя и группы */
-    text_offset += sprintf(text_offset, "%s %s ", getpwuid(file_info->f_stat.st_uid)->pw_name,
-                            getgrgid(file_info->f_stat.st_gid)->gr_name);
+    text_offset += sprintf(text_offset, "%*s %*s ", userAlign, getpwuid(file_info->f_stat.st_uid)->pw_name,
+                                                    groupAlign, getgrgid(file_info->f_stat.st_gid)->gr_name);
     /* печать размера файла */
-    text_offset += sprintf(text_offset, "%5ld ", file_info->f_stat.st_size);
+    text_offset += sprintf(text_offset, "%*ld ", sizeAlign, file_info->f_stat.st_size);
     /* печать времени последнего изменения */
-    local_time = localtime(&(file_info->f_stat.st_mtim.tv_sec));
-    text_offset += strftime(text_offset, NAME_MAX, "%b %-2d %H:%M ", local_time);
+    local_time = *localtime(&(file_info->f_stat.st_mtim.tv_sec));
+    text_offset += (local_time.tm_year == global_time.tm_year)
+                ? strftime(text_offset, NAME_MAX, "%b %-2d %H:%M ", &local_time)
+                : strftime(text_offset, NAME_MAX, "%b %-2d %-5Y ", &local_time);
     /* печать имени файла */
     text_offset += putGayText(text_offset, file_info->name, file_type,
                                 (file_info->f_stat.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)));
@@ -232,7 +281,7 @@ void printStat(const file_info_st* file_info)
     fprintf(stdout, text);
 }
 
-int putGayText(char* out_text, const char* in_text, char file_type, int exec_type)
+int putGayText(char *out_text, const char *in_text, char file_type, int exec_type)
 {
     int offset = 0;
 
@@ -243,10 +292,10 @@ int putGayText(char* out_text, const char* in_text, char file_type, int exec_typ
             offset = sprintf(out_text, BASH_LYELLOW BASH_BGBLACK "%s" BASH_DEFAULT "\n", in_text);
             break;
         case 'd':
-            offset = sprintf(out_text, BASH_BLUE "%s" BASH_DEFAULT "/\n", in_text);
+            offset = sprintf(out_text, BASH_LBLUE "%s" BASH_DEFAULT "/\n", in_text);
             break;
         case 'l':
-            offset = sprintf(out_text, BASH_CYAN "%s" BASH_DEFAULT " -> ", in_text);
+            offset = sprintf(out_text, BASH_LCYAN "%s" BASH_DEFAULT " -> ", in_text);
             break;
         case 'p':
             offset = sprintf(out_text, BASH_YELLOW BASH_BGBLACK "%s" BASH_DEFAULT "|\n", in_text);
@@ -265,13 +314,13 @@ int putGayText(char* out_text, const char* in_text, char file_type, int exec_typ
     return offset;
 }
 
-char getFileType(const char* path)
+char getFileType(const char *path)
 {
     char file_type;
     struct stat f_stat;
 
     lstat(path, &f_stat);
-    if ((f_stat.st_mode & S_IFMT) == S_IFLNK)        file_type = 'l';
+    if      ((f_stat.st_mode & S_IFMT) == S_IFLNK)   file_type = 'l';
     else if ((f_stat.st_mode & S_IFMT) == S_IFCHR)   file_type = 'c';
     else if ((f_stat.st_mode & S_IFMT) == S_IFDIR)   file_type = 'd';
     else if ((f_stat.st_mode & S_IFMT) == S_IFBLK)   file_type = 'b';
@@ -282,7 +331,7 @@ char getFileType(const char* path)
     return file_type;
 }
 
-void signalHandler(int signal, siginfo_t* signalInfo, void* userContext)
+void signalHandler(int signal, siginfo_t *signalInfo, void *userContext)
 {
     if (signal == SIGSEGV)
     {
@@ -330,4 +379,30 @@ void signalCatcher(void)
     signalAction.sa_flags = SA_SIGINFO;
 
     sigaction(SIGSEGV, &signalAction, NULL);
+}
+
+void setAlignment(const file_info_st *file_info)
+{
+    char buff[NAME_MAX];
+    int tmp;
+
+    sprintf(buff,
+#ifdef ARM
+        "%d ",
+#else
+        "%ld ",
+#endif
+        file_info->f_stat.st_nlink);
+    tmp = strlen(buff);
+    linkAlign = linkAlign < tmp ? tmp : linkAlign;
+
+    tmp = strlen(getpwuid(file_info->f_stat.st_uid)->pw_name);
+    userAlign = userAlign < tmp ? tmp : userAlign;
+
+    tmp = strlen(getgrgid(file_info->f_stat.st_gid)->gr_name);
+    groupAlign = groupAlign < tmp ? tmp : groupAlign;
+
+    sprintf(buff, "%ld", file_info->f_stat.st_size);
+    tmp = strlen(buff);
+    sizeAlign = sizeAlign < tmp ? tmp : sizeAlign;
 }
